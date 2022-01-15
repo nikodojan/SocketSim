a0560a275804399080e20240f86126160687cad2
Niko D <nikodojan@live.de>
2022-01-15 17:58:28 +0100

    testproject added

---
+++ SocketSimTests/ParsingHelperTests.cs
@@ -0,0 +1,19 @@
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SocketSimTests
+{
+    [TestClass]
+    public class ParsingHelperTests
+    {
+        [TestMethod]
+        public void TryParse_ValidData_Test()
+        {
+            string ip = "127.0.0.1";
+            string port = "1234";
+            IPEndPoint ep = null;
+            
+
+        }
+    }
+}
